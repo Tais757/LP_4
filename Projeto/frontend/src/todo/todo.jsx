90012dd9b423808a88bd5be7d24752590d172d21
Taís <lara.tlda@gmail.com>
2020-04-29 16:45:17 -0300

    aula da semana 27/04 - 01/04

--- Projeto/frontend/src/todo/todo.jsx
+++ Projeto/frontend/src/todo/todo.jsx
@@ -1,16 +1,42 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+
 import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
 
+const URL = 'http://localhost:3001/api/todos'
+
 export default class Todo extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = { description: '', list: [] } // passando estados vazios
+
+        this.handleAdd = this.handleAdd.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(e) {
+        this.setState({ ...this.state, description: e.target.value })
+    }
+
+    handleAdd() {
+        const description = this.state.description
+        axios.post(URL, {description})
+            .then(resp => console.log('Funcionou!'))
+    }
+
     render() {
         return(
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-                <TodoForm />
+                <TodoForm description={this.state.description} 
+                    handleAdd={this.handleAdd}
+                    handleChange={this.handleChange} />
                 <TodoList />
             </div>
         )
-    }
+    }   
 }
